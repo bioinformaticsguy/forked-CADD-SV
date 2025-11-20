e4c7859aa1fc620638f27170eb45cdc045aff803
bioinformaticsguy <bioinformaticsguy@gmail.com>
2025-11-20 15:16:28 +0100

    Refine VCF to BED conversion in prep_n_run.sh by updating filters and improving chromosome boundary handling

--- prep_n_run.sh
+++ prep_n_run.sh
@@ -32,20 +32,30 @@ base=$(basename "$input_vcf" .vcf.gz)
 base_dir=$(dirname "$input_vcf")
 bed_file="input/${base}.bed"
 
+
+#   | grep -v -E $'\t(INV|BND|DUP:INV|DUP:TANDEM)$' \
+
 # Convert VCF to BED with filters
 echo "Converting and sorting VCF to BED: $input_vcf -> $bed_file"
 bcftools query -f '%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' "$input_vcf" \
   | awk '$2 && $3 && $4 {print $1"\t"$2"\t"$3"\t"$4}' \
-  | grep -v -E $'\t(INV|BND|DUP:INV|DUP:TANDEM)$' \
+  | grep -v -E $'\t(INV|BND)$' \
+  | sed 's/\tDUP:INV$/\tDUP/' | sed 's/\tDUP:TANDEM$/\tDUP/' \
   | grep -E '^chr([1-9]|1[0-9]|2[0-2]|X|Y)[[:space:]]' \
   | grep -vE '_|random|alt|fix|hap|GL|KI' \
   | awk '$2 <= $3' \
   | sort -k1,1 -k2,2n \
   > "$bed_file"
 
+# this remves all the enteries whos end is beyond chrom length
+# awk 'NR==FNR{a[$1]=$2; next} ($2 < $3) && ($3 <= a[$1])' "$chrom_len_file" "$bed_file" > "$base_dir/filtered.bed"
+# mv "$base_dir/filtered.bed" "$bed_file"
 
-awk 'NR==FNR{a[$1]=$2; next} ($2 < $3) && ($3 <= a[$1])' "$chrom_len_file" "$bed_file" > "$base_dir/filtered.bed"
-mv "$base_dir/filtered.bed" "$bed_file"
+## this edits the entries to fit within chrom length and also removes for start >= length of chromosome
+# Clip BED entries to chromosome boundaries
+awk 'NR==FNR{a[$1]=$2; next} {if($3 > a[$1]) $3 = a[$1]; if($2 > a[$1]) $2 = a[$1]; if($2 < $3) print $1"\t"$2"\t"$3"\t"$4}' "$chrom_len_file" "$bed_file" > "$base_dir/clipped.bed"
+mv "$base_dir/clipped.bed" "$bed_file"
+
 
 # Count lines in the filtered BED file
 bed_lines=$(wc -l < "$bed_file")
